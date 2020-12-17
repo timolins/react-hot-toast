6a1fd897168be7b94f8e904dc992f088e35bd7b1
Timo Lins <me@timo.sh>
2020-12-17 19:53:12 +0100

    Update footer

--- site/components/sections/footer.tsx
+++ site/components/sections/footer.tsx
@@ -3,26 +3,31 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className="container relative justify-center my-8 flex gap-8">
-      <span>© 2020 react-hot-toast</span>
-      <span>
-        <span>Made by </span>
-        <a className="underline" href="https://timo.sh">
-          Timo Lins
+    <footer className="container relative justify-center my-8 flex flex-col items-center space-y-4">
+      <div className="flex space-x-4">
+        <a
+          className="underline"
+          href="https://github.com/timolins/react-hot-toast"
+        >
+          GitHub
         </a>
-      </span>
-      <a
-        className="underline"
-        href="https://github.com/timolins/react-hot-toast"
-      >
-        GitHub
-      </a>
-      <Link href="/docs">
-        <a className="underline">Docs</a>
-      </Link>
-      <a className="underline" href="https://twitter.com/timolins">
-        Twitter
-      </a>
+        <Link href="/docs">
+          <a className="underline">Docs</a>
+        </Link>
+        <a className="underline" href="https://twitter.com/timolins">
+          Twitter
+        </a>
+      </div>
+      <div className="text-toast-600">
+        <span>© 2020 react-hot-toast</span>
+        {' · '}
+        <span>
+          <span>Built by </span>
+          <a className="underline" href="https://timo.sh">
+            Timo Lins
+          </a>
+        </span>
+      </div>
     </footer>
   );
 }
