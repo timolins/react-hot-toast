23d3db52e581dfa35a85a10a6395434854b1f84f
Timo Lins <me@timo.sh>
2021-05-28 14:02:04 +0200

    Fix prettier

--- src/core/store.ts
+++ src/core/store.ts
@@ -15,33 +15,33 @@ export enum ActionType {
 
 type Action =
   | {
-    type: ActionType.ADD_TOAST;
-    toast: Toast;
-  }
+      type: ActionType.ADD_TOAST;
+      toast: Toast;
+    }
   | {
-    type: ActionType.UPSERT_TOAST;
-    toast: Toast;
-  }
+      type: ActionType.UPSERT_TOAST;
+      toast: Toast;
+    }
   | {
-    type: ActionType.UPDATE_TOAST;
-    toast: Partial<Toast>;
-  }
+      type: ActionType.UPDATE_TOAST;
+      toast: Partial<Toast>;
+    }
   | {
-    type: ActionType.DISMISS_TOAST;
-    toastId?: string;
-  }
+      type: ActionType.DISMISS_TOAST;
+      toastId?: string;
+    }
   | {
-    type: ActionType.REMOVE_TOAST;
-    toastId?: string;
-  }
+      type: ActionType.REMOVE_TOAST;
+      toastId?: string;
+    }
   | {
-    type: ActionType.START_PAUSE;
-    time: number;
-  }
+      type: ActionType.START_PAUSE;
+      time: number;
+    }
   | {
-    type: ActionType.END_PAUSE;
-    time: number;
-  };
+      type: ActionType.END_PAUSE;
+      time: number;
+    };
 
 interface State {
   toasts: Toast[];
@@ -55,16 +55,13 @@ const addToRemoveQueue = (toastId: string) => {
     return;
   }
 
-  const timeout = setTimeout(
-    () => {
-      toastTimeouts.delete(toastId);
-      dispatch({
-        type: ActionType.REMOVE_TOAST,
-        toastId: toastId,
-      });
-    },
-    1000
-  );
+  const timeout = setTimeout(() => {
+    toastTimeouts.delete(toastId);
+    dispatch({
+      type: ActionType.REMOVE_TOAST,
+      toastId: toastId,
+    });
+  }, 1000);
 
   toastTimeouts.set(toastId, timeout);
 };
@@ -120,9 +117,9 @@ export const reducer = (state: State, action: Action): State => {
         toasts: state.toasts.map((t) =>
           t.id === toastId || toastId === undefined
             ? {
-              ...t,
-              visible: false,
-            }
+                ...t,
+                visible: false,
+              }
             : t
         ),
       };
