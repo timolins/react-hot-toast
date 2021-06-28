06800aa5026c8dca7bf789573a4eaf0919708e86
PJ <pj.sachdev@cite.co.uk>
2021-06-28 21:46:03 +0100

    Add aria-live attribute to Toaster container to ensure toast is read out by screen readers

--- src/components/toaster.tsx
+++ src/components/toaster.tsx
@@ -87,6 +87,7 @@ export const Toaster: React.FC<ToasterProps> = ({
       className={containerClassName}
       onMouseEnter={handlers.startPause}
       onMouseLeave={handlers.endPause}
+      aria-live="polite"
     >
       {toasts.map((t) => {
         const toastPosition = t.position || position;
