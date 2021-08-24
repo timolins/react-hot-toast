b1d2382c93818b2c4f0c1e33c1c5b46d559eed5d
Timo Lins <me@timo.sh>
2021-08-24 12:21:05 +0200

    Fix linting error

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -21,7 +21,7 @@ export const prefersReducedMotion = (() => {
   let shouldReduceMotion: boolean | undefined = undefined;
 
   return () => {
-    if (shouldReduceMotion === undefined && typeof window !== "undefined") {
+    if (shouldReduceMotion === undefined && typeof window !== 'undefined') {
       const mediaQuery = matchMedia('(prefers-reduced-motion: reduce)');
       shouldReduceMotion = !mediaQuery || mediaQuery.matches;
     }
