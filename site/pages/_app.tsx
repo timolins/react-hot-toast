9c384247e6904cdfbb844a18a604afb1dd16fe80
Timo Lins <me@timo.sh>
2024-12-30 12:09:26 +0100

    Add links to headers

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -15,6 +15,48 @@ const components = {
       <a {...props} />
     </Link>
   ),
+  h1: (props) => {
+    const id = props.id || '';
+    return (
+      <h1 {...props}>
+        <Link href={`#${id}`}>
+          <a
+            className={`!no-underline !font-extrabold !text-toast-900 *:!text-toast-900`}
+          >
+            {props.children}
+          </a>
+        </Link>
+      </h1>
+    );
+  },
+  h2: (props) => {
+    const id = props.id || '';
+    return (
+      <h2 {...props}>
+        <Link href={`#${id}`}>
+          <a
+            className={`!no-underline !font-semibold !text-toast-800 *:!text-toast-800`}
+          >
+            {props.children}
+          </a>
+        </Link>
+      </h2>
+    );
+  },
+  h3: (props) => {
+    const id = props.id || '';
+    return (
+      <h3 {...props}>
+        <Link href={`#${id}`}>
+          <a
+            className={`!no-underline !font-semibold !text-toast-800 *:!text-toast-800`}
+          >
+            {props.children}
+          </a>
+        </Link>
+      </h3>
+    );
+  },
   code: (props) =>
     props.className ? (
       <Code className={props.className} snippet={props.children} />
